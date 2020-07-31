9dcb8d0f868c28860ff92786201277a48d9e0ba2
Esteban <estebangs9@gmail.com>
2020-07-30 22:33:56 -0300

    Simple endpoint for creating files on disk.

---
+++ CalzadosLunghi.API/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using CalzadosLunghi.API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalzadosLunghi.API.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FileController : ControllerBase
+    {
+        public const string FilePath = "C:/Users/Esteban/Documents/filesTest/";
+
+        [HttpPost]
+        public ActionResult CreateFile(FileValuesDto fileValuesDto)
+        {
+            var path = String.Concat(FilePath, fileValuesDto.FileName);
+
+            
+            var logFile = System.IO.File.Create(path);
+            var logWriter = new System.IO.StreamWriter(logFile);
+
+            logWriter.WriteLine($"Your file was created at: {path}");
+            logWriter.WriteLine($"Your text: {fileValuesDto.Text}");
+
+            logWriter.Dispose();
+
+            return Ok();
+        }        
+    }
+}
---
+++ CalzadosLunghi.API/DTO/FileValuesDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalzadosLunghi.API.DTO
+{
+    public class FileValuesDto
+    {
+        public string FileName { get; set; }
+        public string Text { get; set; }
+    }
+}
